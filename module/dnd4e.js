e900d0031cfbf062df349816d85a441f88615309
mbutler <matthewtbutler@gmail.com>
2020-02-24 19:51:15 -0600

    basic start

---
+++ module/dnd4e.js
@@ -0,0 +1,26 @@
+/**
+ * Dungeons & Dragons 4th Edition
+ * Author: mbutler
+ * Software License: MIT
+ */
+
+// Import Modules
+import { Dnd4eSheet } from "./character-sheet.js";
+
+/* -------------------------------------------- */
+/*  Foundry VTT Initialization                  */
+/* -------------------------------------------- */
+
+Hooks.once("init", async function() {
+  console.log(`Initializing DnD4e`);
+
+	/**
+	 * Set an initiative formula for the system
+	 * @type {String}
+	 */
+	CONFIG.initiative.formula = "1d20";
+
+  // Register sheet application classes
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("phoenixcommand", Dnd4eSheet, { makeDefault: true });
+});
